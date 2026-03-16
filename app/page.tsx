2a30bbccd33ea0d629b04911f31a5553ecd68e9e
Bongane Lebopo <blebopo@dvtsoftware.com>
2026-03-16 19:32:25 +0200

    Completed Chapter 7

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import AcmeLogo from "@/app/ui/acme-logo";
 import { ArrowRightIcon } from "@heroicons/react/24/outline";
 import Link from "next/link";
 import { lusitana } from "./ui/fonts";
-import Image from "next/image"
+import Image from "next/image";
 
 export default function Page() {
   return (
@@ -32,11 +32,11 @@ export default function Page() {
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
           {/* Add Hero Images Here */}
           <Image
-             src="/hero-desktop.png"
-             width={1000}
-             height={760}
-             className="hidden md:block"
-             alt="Screenshots of the dasboard project showing desktop version"
+            src="/hero-desktop.png"
+            width={1000}
+            height={760}
+            className="hidden md:block"
+            alt="Screenshots of the dasboard project showing desktop version"
           />
           <Image
             src="/hero-mobile.png"
